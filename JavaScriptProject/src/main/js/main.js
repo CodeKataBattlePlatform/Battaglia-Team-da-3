7ef4655c653153c7e18a5e2ee624365e51457687
CodeKataBattlePlatform <157059267+CodeKataBattlePlatform@users.noreply.github.com>
2024-02-14 20:45:09 +0100

    Create JavaScriptProject/src/main/js folder

---
+++ JavaScriptProject/src/main/js/main.js
@@ -0,0 +1,7 @@
+// main.test.js
+function logHelloWorld() {
+    console.log('Hello, World in JS!');
+}
+
+// If you have other code in main.test.js, make sure to export the necessary functions or variables.
+module.exports = { logHelloWorld };
